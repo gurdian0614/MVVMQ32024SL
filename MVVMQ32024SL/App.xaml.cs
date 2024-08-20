083b51c3cdfbf3fdcb691dbd0cd10eaa50ca792e
gurdian0614 <ivan.gurdian@outlook.com>
2024-08-19 19:54:46 -0600

    Add project files.

---
+++ MVVMQ32024SL/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MVVMQ32024SL
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MVVMQ32024SL/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MVVMQ32024SL
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
